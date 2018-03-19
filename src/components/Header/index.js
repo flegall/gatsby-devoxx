4dab53a7876efdc186322ef56426862e3895beb0
Florent Le Gall <florent.legall@gmail.com>
2018-03-19 13:42:30 +0100

    feat(styled): Add styled components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Background = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Title = styled.h1`
+  margin: 0;
+`
+
+const WhiteLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Devoxx recipes !
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <Background>
+    <Container>
+      <Title>
+        <WhiteLink to="/">Devoxx recipes !</WhiteLink>
+      </Title>
+    </Container>
+  </Background>
 )
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
 import Header from '../components/Header'
 import './index.css'
+
+const Container = styled.div`
+  margin: 0px auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0px;
+`
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -15,16 +23,7 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Container>{children()}</Container>
   </div>
 )
 
